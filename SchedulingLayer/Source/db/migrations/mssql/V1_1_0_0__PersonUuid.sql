7c8a175a8a1a61d338ed190d7a57221e113ae161
Brian Storm Graversen <bsg@digital-identity.dk>
2025-07-25 11:43:08 +0200

    fixed issue with bad SQL migration for MSSQL

--- SchedulingLayer/Source/db/migrations/mssql/V1_1_0_0__PersonUuid.sql
+++ SchedulingLayer/Source/db/migrations/mssql/V1_1_0_0__PersonUuid.sql
@@ -1,3 +1,3 @@
-ALTER TABLE queue_users ADD COLUMN person_uuid VARCHAR(36) NULL;
-ALTER TABLE success_users ADD COLUMN person_uuid VARCHAR(36) NULL;
-ALTER TABLE failure_users ADD COLUMN person_uuid VARCHAR(36) NULL;
+ALTER TABLE queue_users ADD person_uuid VARCHAR(36) NULL;
+ALTER TABLE success_users ADD person_uuid VARCHAR(36) NULL;
+ALTER TABLE failure_users ADD person_uuid VARCHAR(36) NULL;
--- SchedulingLayer/Source/db/migrations/mssql/V1_1_2_0__Robot.sql
+++ SchedulingLayer/Source/db/migrations/mssql/V1_1_2_0__Robot.sql
@@ -1,3 +1,3 @@
-ALTER TABLE queue_users ADD COLUMN is_robot BIT NOT NULL DEFAULT 0;
-ALTER TABLE success_users ADD COLUMN is_robot BIT NULL;
-ALTER TABLE failure_users ADD COLUMN is_robot BIT NULL;
+ALTER TABLE queue_users ADD is_robot BIT NOT NULL DEFAULT 0;
+ALTER TABLE success_users ADD is_robot BIT NULL;
+ALTER TABLE failure_users ADD is_robot BIT NULL;
